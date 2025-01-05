d7399d02c9f9dc45c558aec9f5b556a675f68537
Siddharth Chaini <40721514+sidchaini@users.noreply.github.com>
2025-01-06 01:22:26 +0530

    Added choice for plotting distance space

--- utils.py
+++ utils.py
@@ -10,30 +10,30 @@ def get_metric_name(metric):
         metric_str = metric
     return metric_str.title()
 
-def visualize_distance(metric: str):
-    metric_str = metric
+def visualize_distance(metric: str, ref_point=(5, 5), grid_range=(0, 10)):
+    ref_point = np.array(ref_point)
+    metric_str = get_metric_name(metric)
 
-    ref_point = np.array([5, 5])
-    x = np.linspace(0, 10, 100)
-    y = np.linspace(0, 10, 100)
+    x = np.linspace(grid_range[0], grid_range[1], 100)
+    y = np.linspace(grid_range[0], grid_range[1], 100)
     X, Y = np.meshgrid(x, y)
     points = np.vstack([X.ravel(), Y.ravel()]).T
-    
-    metric_fn_, metric = dcpy.classifier.initialize_metric_function(metric_str)
+
+    metric_fn_, metric = dcpy.classifier.initialize_metric_function(metric)
 
     distances = distance.cdist(ref_point.reshape(1, -1), points, metric=metric)
     distances = distances.reshape(X.shape)
     
-    fig, ax = plt.subplots(1,1, figsize=(4,4), sharex=False, sharey=False)
+    fig, ax = plt.subplots(1, 1, figsize=(4, 4), sharex=False, sharey=False)
     im = ax.imshow(
         distances,
         cmap="Blues",
         interpolation="nearest",
-        extent=[0, 10, 0, 10],
+        extent=[grid_range[0], grid_range[1], grid_range[0], grid_range[1]],
         origin="lower",
     )
 
-    ax.scatter(5, 5, s=50, color="k", marker="x")
+    ax.scatter(ref_point[0], ref_point[1], s=50, color="k", marker="x")
     cs = ax.contour(X, Y, distances, colors="k")
     ax.clabel(cs, inline=True, fontsize=10, colors="k")
     ax.set_title(f"{metric_str.title().replace('_','-')}")
