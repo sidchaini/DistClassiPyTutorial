7b97d8acc25c595529abc640f724062383030fe0
Siddharth Chaini <40721514+sidchaini@users.noreply.github.com>
2025-01-06 12:26:32 +0530

    black formatting

--- utils.py
+++ utils.py
@@ -3,12 +3,14 @@ from scipy.spatial import distance
 import distclassipy as dcpy
 import matplotlib.pyplot as plt
 
+
 def get_metric_name(metric):
     if callable(metric):
         metric_str = metric.__name__
     else:
         metric_str = metric
     return metric_str.title()
+
 
 def visualize_distance(metric: str, ref_point=(5, 5), grid_range=(0, 10)):
     ref_point = np.array(ref_point)
@@ -23,7 +25,7 @@ def visualize_distance(metric: str, ref_point=(5, 5), grid_range=(0, 10)):
 
     distances = distance.cdist(ref_point.reshape(1, -1), points, metric=metric)
     distances = distances.reshape(X.shape)
-    
+
     fig, ax = plt.subplots(1, 1, figsize=(4, 4), sharex=False, sharey=False)
     im = ax.imshow(
         distances,
@@ -37,5 +39,5 @@ def visualize_distance(metric: str, ref_point=(5, 5), grid_range=(0, 10)):
     cs = ax.contour(X, Y, distances, colors="k")
     ax.clabel(cs, inline=True, fontsize=10, colors="k")
     ax.set_title(f"{metric_str.title().replace('_','-')}")
-    
+
     return ax
